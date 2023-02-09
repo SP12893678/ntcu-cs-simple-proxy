b9163ea582c83a7b3bdefeccb4b71b02e31606fc
SP12893678 <36910625+SP12893678@users.noreply.github.com>
2023-02-09 14:57:42 +0800

    feat: 新增SSL憑證更新重整機制

--- nginxReloader.sh
+++ nginxReloader.sh
@@ -5,6 +5,9 @@ nginx -g daemon off;
 while true
 do
   inotifywait --exclude .swp -e create -e modify -e delete -e move /etc/nginx/conf.d
+  inotifywait --exclude .swp -e create -e modify -e delete -e move /etc/nginx/ssl/server.cer
+  inotifywait --exclude .swp -e create -e modify -e delete -e move /etc/nginx/ssl/server.key
+  inotifywait --exclude .swp -e create -e modify -e delete -e move /etc/nginx/ssl/uca.cer
   nginx -t
   if [ $? -eq 0 ]
   then
